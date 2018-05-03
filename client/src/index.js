983a31092e37f30b2cd4874a3f9b93fe57076bcd
Drillibit <293344asd@gmail.com>
2018-05-03 10:11:12 +0600

    disable server worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,7 @@ import { Provider } from 'react-redux';
 import storeConfig from './store/storeConfig';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 const store = storeConfig();
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
